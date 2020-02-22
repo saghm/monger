6d0d3e3c2cd6b64361cbbb14c44e7047ce3f0f2a
Saghm Rossi <saghmrossi@gmail.com>
2020-02-22 16:32:12 -0500

    upgrade dependencies

--- monger-core/src/error.rs
+++ monger-core/src/error.rs
@@ -1,70 +1,66 @@
-use err_derive::Error;
+use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error(
-        display = "Unable to find binary to execute. Run `monger get {}` and try again if you're \
-                   sure the version and binary name are correct",
-        version
+        "Unable to find {binary} to execute. Run `monger get {version}` and try again if you're \
+         sure the version and binary name are correct"
     )]
     BinaryNotFound { binary: String, version: String },
 
-    #[error(display = "`{}` command failed", command)]
+    #[error("`{command}` command failed")]
     FailedSubprocess {
         command: String,
         exit_code: Option<i32>,
     },
 
-    #[error(display = "An HTTP error occurred: {}", inner)]
+    #[error("An HTTP error occurred: {inner}")]
     Http {
-        #[error(cause)]
+        #[from]
         inner: reqwest::Error,
     },
 
-    #[error(
-        display = "HTML response from {} did not match expected structure",
-        url
-    )]
+    #[error("HTML response from {url} did not match expected structure")]
     InvalidHtml { url: String },
 
-    #[error(display = "MongoDB version {} does not exist", version)]
+    #[error("MongoDB version {version} does not exist")]
     InvalidVersion { version: String },
 
-    #[error(display = "An I/O error occurred: {}", inner)]
+    #[error("An I/O error occurred: {inner}")]
     Io {
-        #[error(cause)]
+        #[from]
         inner: std::io::Error,
     },
 
-    #[error(display = "Unable to determine the OS release version")]
+    #[error("Unable to determine the OS release version")]
     OsRelease {
-        #[error(cause)]
+        #[from]
         inner: rs_release::OsReleaseError,
     },
 
-    #[error(display = "Unable to parse semantic version")]
+    #[error("Unable to parse semantic version")]
     SemVer {
-        #[error(cause)]
+        #[from]
         inner: semver::SemVerError,
     },
 
-    #[error(display = "Unable to convert HTTP header to string: {}", inner)]
+    #[error("Unable to convert HTTP header to string: {inner}")]
     ToStr {
-        #[error(cause)]
+        #[from]
         inner: reqwest::header::ToStrError,
     },
 
-    #[error(display = "Unable to find home directory")]
+    #[error("Unable to find home directory")]
     UnknownHomeDirectory,
 
-    #[error(display = "Unable to identify operating system")]
+    #[error("Unable to identify operating system")]
     UnknownOs,
 
-    #[error(display = "{} is unsupported", os_name)]
+    #[error("{os_name} is unsupported")]
     UnsupportedOs { os_name: String },
 
-    #[error(display = "Unable to find version {}", version)]
+    #[error("Unable to find version {version}")]
     VersionNotFound { version: String },
 }
